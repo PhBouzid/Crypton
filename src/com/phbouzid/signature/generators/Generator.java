abd737972d4fb1e17aa7e1ded041f168d0a1cc2d
PhBouzid <sindbad342@gmail.com>
2015-12-04 13:10:39 +0600

    Create package and some empty classes

---
+++ src/com/phbouzid/signature/generators/Generator.java
@@ -0,0 +1,8 @@
+package com.phbouzid.signature.generators;
+
+/**
+ * Created by Y510P on 04.12.2015.
+ */
+public interface Generator<T> {
+    public T generate();
+}
---
+++ src/com/phbouzid/signature/generators/cms/PKCSGenerator.java
@@ -0,0 +1,8 @@
+package com.phbouzid.signature.generators.cms;
+
+/**
+ * Created by Y510P on 04.12.2015.
+ */
+public class PKCSGenerator {
+
+}
